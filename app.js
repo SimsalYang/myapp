0b015b337e4b5f2d8a8682844ae0e6105825d139
Simon Yang <nicolas.yoong@gmail.com>
2017-11-30 21:05:16 +0800

    add db

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('./models/init');//连接数据库
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
--- route.api.js
+++ route.api.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var PostModel = require('./models/post');
 
 /** GET users lists */
 router.get('/users',function(req,res,next){
@@ -8,7 +9,14 @@ router.get('/users',function(req,res,next){
 
 /** GET posts lists */
 router.get('/posts',function(req,res,next){
-  res.json({postsList:['文章1','文章2','文章3']});
+  PostModel.find({}, {}, function (err, posts) {
+    if (err) {
+      res.json({ success: false });
+      return;
+    }
+
+    res.json({ success: true, postsList: posts });
+  });
 });
 
 /** POST posts */
@@ -16,6 +24,19 @@ router.post('/posts/create',function(req,res,next){
   var title = req.body.title;
   var content = req.body.content;
   res.send({title,content});//收到数据后，把数据返回给请求方
+});
+
+/** POST create post */
+router.post('/posts/create', function (req, res, next) {
+  var title = req.body.title;
+  var content = req.body.content;
+
+  var post = new PostModel();
+  post.title = title;
+  post.content = content;
+  post.save(function (err, doc) {
+    res.json({ success: true });
+  });
 });
 
 module.exports = router;
