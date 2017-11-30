0b5394c61f04ad430884055f4db585f6cefd45bf
Simon Yang <nicolas.yoong@gmail.com>
2017-11-30 22:03:25 +0800

    add markdown display

--- route.page.js
+++ route.page.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var PostModel = require('./models/post');
+var marked = require('marked');
 
 /** GET home page */
 router.get('/',function(req,res,next){
@@ -17,6 +18,7 @@ router.get('/posts/show', function (req, res, next) {
   var id = req.query.id;
 
   PostModel.findOne({ _id: id }, function (err, post) {
+    post.content = marked(post.content);
     res.render('show', { post });
   });
 });
